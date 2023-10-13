d40baff75eb23be06c038b3181bd8356121b43c1
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-10-13 23:43:50 +0900

    Data-table-component  Simple completed

--- src/app/users/columns.tsx
+++ src/app/users/columns.tsx
@@ -1,24 +1,63 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
-
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export type User = {
-  id: string
-  name: string
-  email: string
-  image: string
-  lastSeen: string
-}
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  lastSeen: string;
+};
 
 export const columns: ColumnDef<User>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
     accessorKey: "name",
-    header: "Name",
-    cell:({row})=>{
-      const name: string = row.getValue('name');
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const name: string = row.getValue("name");
       return name.split(".").join(" ");
-    }
+    },
   },
   {
     accessorKey: "email",
@@ -27,10 +66,37 @@ export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "lastSeen",
     header: "Last Seen",
-    cell: ({row}) => {
-      const date = new Date(row.getValue('lastSeen'))
-      const formatted = date.toLocaleDateString()
-      return <div className="font-medium">{formatted}</div>
-    }
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("lastSeen"));
+      const formatted = date.toLocaleDateString();
+      return <div className="font-medium">{formatted}</div>;
+    },
   },
-]
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const user = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(user.id)}
+            >
+              Copy user ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
